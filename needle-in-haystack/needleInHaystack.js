5686dfe7fd5cc9ad1089f3d57f11ab61e431a83d
MatthewFavela <mfavela3508@myhta.org>
2018-12-05 11:48:15 -1000

    update

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,8 @@
 module.exports = function needleInHaystack(haystack) {
-  // write code in here
-}
+  let needle = haystack.includes("needle")
+  if(needle === true){
+    return haystack.indexOf("needle");
+  } else {
+    return false;
+  }
+} 
